ac22f994dc5777ea987dcbd9185483a8205cda11
unknown <amankumarsharma345@gmail.com>
2022-02-20 00:26:41 +0530

    Setup file experimental

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="Cpu Ram Grabber",
+    
+    # Version number (initial):
+    version="0.1.0",
+    
+    # Application author details:
+    author="Aman Sharma",
+    author_email="amankumarsharma345@gmail.com",
+    
+    
+    # Include additional files into the package
+    include_package_data=True,
+    
+   
+    #
+    # license="LICENSE.txt",
+    description="Ram and Cpu use displayer",
+    
+    
+    
+    # Dependent packages (distributions)
+    install_requires=[
+        "PySide2",
+        "PySide2extn",
+        "psutil"
+    ],
+)
